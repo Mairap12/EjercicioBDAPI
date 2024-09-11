0284e1bbaa516ae60b35a8d6059aa639b0d96b0a
Mairap12 <mairanallelyperezcortez@gmail.com>
2024-09-11 08:45:09 -0600

    Subir DBAPI

---
+++ Models/BdContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIPRUEBAS.Models;
+
+public partial class BdContext : DbContext
+{
+    public BdContext()
+    {
+    }
+
+    public BdContext(DbContextOptions<BdContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categoria { get; set; }
+
+    public virtual DbSet<Producto> Producto { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.IdCategoria).HasName("PK__CATEGORI__A3C02A1003F78CC1");
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProducto).HasName("PK__PRODUCTO__09889210961921E5");
+
+            entity.HasOne(d => d.oCategoria).WithMany(p => p.Producto).HasConstraintName("FK_IDCATEGORIA");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
